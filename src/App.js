81c5e444078eab2a6ebfdb34ca241f08e9fc0d5f
seojinwoong <tjwlsdnd12@summitcorp.co.kr>
2023-04-21 14:36:30 +0900

    로그인한 유저정보 redux에 추가하기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import ChatPage from './components/ChatPage/ChatPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from './redux/actions/user_action';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<ChatPage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/register" element={<RegisterPage/>} />
-    </Routes>
-  )
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.user_reducer.isLoading)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) { // 로그인 한 경우
+        navigate('/');
+        dispatch(setUser(user));        
+      } else { // 로그인 하지 않은 경우
+        navigate('/login');
+      }
+    });
+  }, []);
+  
+  if (isLoading) {
+    return (
+      <div>
+        ...loading
+      </div>
+    )
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<ChatPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+      </Routes>
+    )
+  }
 }
 
 export default App
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import Spinner from 'react-bootstrap/Spinner';
+
+let timer;
 
 const LoginPage = () => {
+  const [errorFromSubmit, setErrorFromSubmit] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+
+      const auth = getAuth();
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+
+      setLoading(false);
+
+    } catch (error) {
+      setErrorFromSubmit(error.message);
+      setLoading(false);
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        setErrorFromSubmit('');
+      }, 5000); 
+    }
+  }
+
   return (
-    <div>LoginPage</div>
-  )
+    <div className='auth-wrapper'>
+      <div style={{ textAlign: 'center' }}>
+        <h3>Register</h3>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>email</label>
+        <input type="email" name='email' { ...register('email', { required: true, pattern: /^\S+@\S+$/i }) }/>
+        { errors.email && errors.email.type === 'required' && <p>이메일은 필수입력 값입니다.</p> }
+        { errors.email && errors.email.type === 'pattern' && <p>유효하지 않은 이메일 형식입니다.</p> }
+
+        <label>password</label>
+        <input type="password" name='password' { ...register('password', { required: true, minLength: 6 }) }/>
+        { errors.password && errors.password.type === 'required' && <p>비밀번호는 필수입력 값입니다.</p> }
+        { errors.password && errors.password.type === 'minLength' && <p>비밀번호는 최소 6자 이상 입력하셔야 합니다.</p> }
+
+        { errorFromSubmit && <p className='error_message'>{errorFromSubmit}</p> }
+
+        <button disabled={loading} >
+          {loading ? <Spinner animation="border" variant="light" /> : '제출'}
+        </button>
+
+        <Link style={{ color: 'gray', textDecoration: 'none' }} to="/register">아직 아이디가 없다면,,,</Link>
+      </form>
+    </div>
+  );
+
 }
+
 
 export default LoginPage
