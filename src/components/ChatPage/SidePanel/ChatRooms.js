b0babfb8b90cb3f00016fb088218991226273c77
seojinwoong <tjwlsdnd12@summitcorp.co.kr>
2023-04-24 11:12:11 +0900

    채팅방 개설창 ui작성

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { FaRegSmileWink, FaPlus } from 'react-icons/fa';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+export class ChatRooms extends Component {
+  state = {
+    modalShow: false,
+  }
+  setModalShow = () => {  this.setState({ modalShow: true }) };
+  setModalHide = () => {  this.setState({ modalShow: false }) };
+  
+  render() {
+    return (
+      <div>
+        <div style={{ position: 'relative', width: '100%', display: 'flex', alignItems: 'center' }}>
+          <FaRegSmileWink style={{ marginRight: 3 }}/>
+          CHAT ROOMS {" "} (1)
+          <FaPlus style={{ position: 'absolute', right: 0, cursor: 'pointer' }} onClick={this.setModalShow}/>
+        </div>
 
-const ChatRooms = () => {
-  return (
-    <div>ChatRooms</div>
-  )
+        <Modal
+          show={this.state.modalShow}
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-vcenter">
+              채팅방 개설하기
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>채팅방 이름</Form.Label>
+                <Form.Control type="text" placeholder="채팅방 이름을 작성해주세요." />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>채팅방 상세설명</Form.Label>
+                <Form.Control as="textarea" placeholder="채팅방 이름을 작성해주세요." />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+             <Button>채팅방 개설</Button>
+            <Button onClick={this.setModalHide}>닫기</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
 }
 
 export default ChatRooms
