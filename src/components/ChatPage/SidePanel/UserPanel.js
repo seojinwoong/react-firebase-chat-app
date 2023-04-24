8d74b2d113ad2b5c1a462b70026223132f0c7c68
seojinwoong <tjwlsdnd12@summitcorp.co.kr>
2023-04-24 10:30:38 +0900

    유저프로필 변경하기 기능구현

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { AiFillWechat } from 'react-icons/ai';
 import Image from 'react-bootstrap/Image';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import { useSelector } from 'react-redux';
-import { getAuth, signOut } from 'firebase/auth';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAuth, signOut, updateProfile } from 'firebase/auth';
+import { getDatabase, ref, update } from 'firebase/database';
+import { getStorage, uploadBytesResumable, ref as strRef, getDownloadURL } from 'firebase/storage';
+import { setPhotoURL } from '../../../redux/actions/user_action';
 
 const UserPanel = () => {
+  const dispatch = useDispatch();
   const user = useSelector(state => state.user_reducer.currentUser);
+  const inputOpenImageRef = useRef();
 
   const handleLogout = () => {
     const auth = getAuth();
     signOut(auth);
   }
+
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  }
+
+  const handleUploadImage = async (e) => {
+    const file = e.target.files[0];
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    const metadata = { contentType: file.type };
+    const storage = getStorage();
+
+    try {
+      let uploadTask = uploadBytesResumable(strRef(storage, `user_image/${user.uid}`), file, metadata);
+
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        updateProfile(user, {
+          photoURL: downloadURL
+        });
+
+        dispatch(setPhotoURL(downloadURL));
+        
+        update(ref(getDatabase(), `users/${user.uid}`), {image: downloadURL});
+
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  } 
 
   return (
     <div>
@@ -26,11 +61,19 @@ const UserPanel = () => {
           : null
         }
 
-        <DropdownButton id="dropdown-basic-button" title={ user ? user.displayName + '님, 반갑습니다!' : null }>
-          <Dropdown.Item>프로필 사진 변경</Dropdown.Item>
+        <DropdownButton id="dropdown-basic-button"  title={ user ? user.displayName + '님, 반갑습니다!' : null }>
+          <Dropdown.Item onClick={handleOpenImageRef}>프로필 사진 변경</Dropdown.Item>
           <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
         </DropdownButton>
       </div>
+
+      <input
+        onChange={handleUploadImage}
+        accept="image/jpeg, image/png"
+        style={{ display: 'none' }}
+        ref={inputOpenImageRef}
+        type="file"
+      />
 
     </div>
   )
--- src/redux/actions/user_action.js
+++ src/redux/actions/user_action.js
@@ -1,6 +1,7 @@
 import {
     SET_USER,
-    CLEAR_USER
+    CLEAR_USER,
+    SET_PHOTO_URL
 } from './types';
 
 export function setUser (user) {
@@ -12,5 +13,11 @@ export function setUser (user) {
 export function clearUser () {
     return {
         type: CLEAR_USER
+    }
+}
+export function setPhotoURL (photoURL) {
+    return {
+        type: SET_PHOTO_URL,
+        payload: photoURL
     }
 }
